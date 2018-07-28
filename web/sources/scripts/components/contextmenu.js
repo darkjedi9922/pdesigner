7d2598e1534f6927ebaec5be7f1ba561b696bfe5
Jed Dark <darkjedi9922@gmail.com>
2018-07-28 11:38:13 +0300

    Улучшает реализацию контекстного меню (его позиционирования, когда ему не хватает места)
    Также разрешает костыли с setTimeout в Vue скриптах

--- web/sources/scripts/components/contextmenu.js
+++ web/sources/scripts/components/contextmenu.js
@@ -3,6 +3,7 @@
         return {
             parent: null,
             exists: false,
+            checked: false,
             x: 0,
             y: 0
         }
@@ -13,15 +14,34 @@
         $(document.body).on('contextmenu', (function (event) {
             if (event.target === this.parent) {
                 this.exists = true;
-                this.x = event.clientX;
-                this.y = event.clientY;
+                this.x = event.clientX + pageXOffset + 1;
+                this.y = event.clientY + pageYOffset;
+                this.$nextTick(this.checkPosition);
                 event.preventDefault();
             } else this.exists = false;
         }).bind(this));
 
         $(document.body).on('click', (function() {
             this.exists = false;
+            this.checked = false;
         }).bind(this));
     },
-    template: '<div v-if="exists" :style="{ left: x + \'px\', top: y + \'px\', position: \'absolute\' }"><slot></slot></div>'
+    methods: {
+        checkPosition: function() {
+            var height = "".split.call($(this.$el).css('height'), 'px')[0];
+            if ((+this.y - +pageYOffset + +height) > window.innerHeight) this.y -= height;
+            var width = "".split.call($(this.$el).css('width'), 'px')[0];
+            if ((+this.x - +pageXOffset + +width) > window.innerWidth) this.x -= width;
+            this.$nextTick(function () {
+                this.checked = true;
+            });
+        }
+    },
+    template: '\
+        <div v-if="exists" :style="{ \
+            left: x + \'px\',\
+            top: y + \'px\',\
+            visibility: (checked ? \'visible\' : \'hidden\'),\
+            position: \'absolute\' }">\
+        <slot></slot></div>'
 });

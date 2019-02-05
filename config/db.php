8acea9e26907c076b8a4d8378dd6585a65caf4ff
Jed Dark <darkjedi9922@gmail.com>
2019-02-05 21:08:52 +0200

    Change db password.

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=pdesigner',
     'username' => 'root',
-    'password' => '',
+    'password' => '0000',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)

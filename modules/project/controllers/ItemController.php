859bad32290b7b1e766c18bb9267c5667db19a65
Jed Dark <darkjedi9922@gmail.com>
2019-07-27 18:05:23 +0300

    Implement project rights.

--- modules/project/controllers/ItemController.php
+++ modules/project/controllers/ItemController.php
@@ -9,10 +9,21 @@ use app\modules\todo\models\Issue;
 use app\modules\project\models\AddProjectForm;
 use app\modules\project\models\EditProjectForm;
 use app\modules\todo\models\IssueGroup;
+use app\modules\project\models\ProjectRights;
+use yii\web\NotFoundHttpException;
 
 class ItemController extends Controller
 {
     public $layout = '@app/views/layouts/dashboard';
+
+    /** @var ProjectRights $rights */
+    public $rights;
+
+    public function init()
+    {
+        parent::init();
+        $this->rights = new ProjectRights(Yii::$app->user->identity);
+    }
 
     /**
      * @param int $id id проекта
@@ -20,11 +31,9 @@ class ItemController extends Controller
      */
     public function actionIndex($id)
     {
-        $project = Project::findOne([
-            'id' => $id,
-            'author_id' => Yii::$app->user->id
-        ]);
-        if (!$project) return Yii::$app->response->statusCode = 404;
+        $project = Project::findOne($id);
+        if (!$project || !$this->rights->canSee($project)) 
+            throw new NotFoundHttpException;
 
         $issues = Issue::find()->where(['project_id' => $id])->orderBy('id ASC')->all();
         $groups = IssueGroup::find()->where(['project_id' => $id])->orderBy('id ASC')->all();
@@ -42,6 +51,8 @@ class ItemController extends Controller
      */
     public function actionAdd()
     {
+        if (!$this->rights->canAdd()) throw new NotFoundHttpException;
+
         $model = new AddProjectForm();
         if ($model->load(Yii::$app->request->post()) && $project = $model->add()) {
             return $this->redirect(['/project', 'id' => $project->id]);
@@ -56,11 +67,9 @@ class ItemController extends Controller
      */
     public function actionEdit($id)
     {
-        $project = Project::findOne([
-            'id' => $id,
-            'author_id' => Yii::$app->user->id
-        ]);
-        if (!$project) return Yii::$app->response->statusCode = 404;
+        $project = Project::findOne($id);
+        if (!$project || !$this->rights->canEdit($project)) 
+            throw new NotFoundHttpException;
 
         if (Yii::$app->request->isPost) {
             $form = new EditProjectForm();
@@ -82,11 +91,11 @@ class ItemController extends Controller
      */
     public function actionDelete($id)
     {
-        $project = Project::findOne([
-            'id' => $id,
-            'author_id' => Yii::$app->user->id
-        ]);
-        if ($project) $project->delete();
+        $project = Project::findOne($id);
+        if (!$project || !$this->rights->canDelete($project))
+            throw new NotFoundHttpException;
+
+        $project->delete();
         $this->redirect(['/dashboard']);
     }
 }

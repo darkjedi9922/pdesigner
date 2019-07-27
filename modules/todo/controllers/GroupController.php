168bfd8a3dab6f371d8b843495cf9663ff330d83
Jed Dark <darkjedi9922@gmail.com>
2019-07-27 18:21:26 +0300

    Add issue group rights.

--- modules/todo/controllers/GroupController.php
+++ modules/todo/controllers/GroupController.php
@@ -4,16 +4,36 @@ namespace app\modules\todo\controllers;
 
 use yii\web\Controller;
 use app\modules\todo\models\IssueGroup;
+use app\modules\todo\models\GroupRights;
+use Yii;
+use yii\web\NotFoundHttpException;
+use app\modules\project\models\Project;
 
 class GroupController extends Controller
 {
+    /** @var GroupRights $rights */
+    public $rights;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->rights = new GroupRights(Yii::$app->user->identity);
+    }
+
     /**
      * @param int $group Id of the group
      * @param int $color Id of the color
      */
     public function actionSetColor($group, $color)
     {
-        IssueGroup::updateAll(['color_id' => $color], 'id =' . $group);
+        $group = IssueGroup::findOne($group);
+        if (!$group || !$this->rights->canSetColor($group))
+            throw new NotFoundHttpException;
+        $group->color_id = $color;
+        $group->save();
     }
 
     /**
@@ -22,7 +42,11 @@ class GroupController extends Controller
      */
     public function actionSetName($group, $name)
     {
-        IssueGroup::updateAll(['name' => $name], 'id =' . $group);
+        $group = IssueGroup::findOne($group);
+        if (!$group || !$this->rights->canSetName($group))
+            throw new NotFoundHttpException;
+        $group->name = $name;
+        $group->save();
     }
 
     /**
@@ -31,6 +55,10 @@ class GroupController extends Controller
      */
     public function actionAdd($project)
     {
+        $projectObject = Project::findOne($project);
+        if (!$projectObject || !$this->rights->canAdd($projectObject))
+            throw new NotFoundHttpException;
+
         $group = new IssueGroup();
         $group->name = 'Новая группа';
         $group->color_id = 4;
@@ -48,11 +76,9 @@ class GroupController extends Controller
      */
     public function actionDelete($group)
     {
-        IssueGroup::remove($group);
-    }
-
-    public function actionIndex()
-    {
-        return 'hello';
+        $group = IssueGroup::findOne($group);
+        if (!$group || !$this->rights->canDelete($group))
+            throw new NotFoundHttpException;
+        $group->delete();
     }
 }

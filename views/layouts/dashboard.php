1a84530e5aaa68fd11ed14a58d88a29e19693bb9
Jed Dark <darkjedi9922@gmail.com>
2019-07-29 20:08:38 +0300

    Add special buttons to menu.

--- views/layouts/dashboard.php
+++ views/layouts/dashboard.php
@@ -10,17 +10,20 @@ $projects = Project::find()
 <?php $this->beginContent('@app/views/layouts/simple.php') ?>
 <div class="container">
     <div class="menu" id="menu">
-        <a href="<?= Url::to(['/']) ?>" class="menu-header">
-            <span class="menu-header__title">PDesigner</span>
-        </a>
-
-        <?php foreach ($projects as $project) : ?>
-            <a href="<?= Url::to(['/project', 'id' => $project['id']]) ?>" class="menu__item">
-                <project-logo name="<?= $project['name'] ?>" class="project-logo--minified"></project-logo>
-                <?= $project['name'] ?>
-            </a>
-        <?php endforeach ?>
-        <a href="<?= Url::to(['/project/item/add']) ?>" class="menu__item">Добавить проект</a>
+        <div class="menu__items">
+            <?php foreach ($projects as $project) : ?>
+                <a href="<?= Url::to(['/project', 'id' => $project['id']]) ?>" class="menu__item">
+                    <project-logo name="<?= $project['name'] ?>" class="project-logo--minified"></project-logo>
+                    <span class="menu__label"><?= $project['name'] ?></span>
+                </a>
+            <?php endforeach ?>
+        </div>
+        <div class="menu__specials">
+            <span class="menu__special menu__special--toggle"><i class="icon chevron left"></i></span>
+            <a href="<?= Url::to(['/']) ?>" class="menu__special menu__special--home"><i class="icon home"></i></a>
+            <a href="<?= Url::to(['/dashboard']) ?>" class="menu__special menu__special--dashboard"><i class="icon cube"></i></a>
+            <a href="<?= Url::to(['/project/item/add']) ?>" class="menu__special menu__special--add"><i class="icon plus circle"></i></a>
+        </div>
     </div>
     <div class="desk">
         <?= $content ?>

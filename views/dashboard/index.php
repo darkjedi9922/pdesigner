8ce8dfa7e310acaff5db4153ced1e0208cfadba4
Jed Dark <darkjedi9922@gmail.com>
2019-07-30 11:32:51 +0300

    Recieve dashboard tasks from db.

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,75 +1,64 @@
+<?php
+$tasks = Yii::$app->db->createCommand(
+    "SELECT [[id]], [[project_id]], [[number]], [[group_id]], [[title]]
+    FROM {{issues}} WHERE [[status]] = 2"
+)->queryAll();
+$projects = [];
+$groups = [];
+?>
+
 <script type="text/javascript">
     _dashboardAppData = {
         username: "<?= Yii::$app->user->identity->username ?>",
-        tasks: {
-            1: {
-                id: 1,
-                number: 41,
-                projectId: 1,
-                title: 'Сделать Dashboard в Project DesignerСделать Dashboard в Project Designer',
-                groupId: 1
-            },
-            2: {
-                id: 2,
-                number: 42,
-                projectId: 1,
-                title: 'Сделать Dashboard в Project DesignerСделать Dashboard в Project Designer',
-                groupId: 1
-            },
-            4: {
-                id: 4,
-                number: 41,
-                projectId: 2,
-                title: 'Сделать Dashboard в Project DesignerСделать Dashboard в Project Designer',
-                groupId: 3
-            },
-            5: {
-                id: 5,
-                number: 42,
-                projectId: 2,
-                title: 'Сделать Dashboard в Project DesignerСделать Dashboard в Project Designer',
-                groupId: 3
-            }
-        },
-        projects: {
-            1: {
-                id: 1,
-                name: 'Lightness',
-                description: '# The description\n\nNew **line**'
-            },
-            2: {
-                id: 2,
-                name: 'Project Designer',
-                description: '# The description\n\nNew **line**'
-            }
-        },
-        groups: {
-            1: {
-                id: 1,
-                projectId: 1,
-                name: 'Новые фичи',
-                colorId: 4
-            },
-            2: {
-                id: 2,
-                projectId: 1,
-                name: 'Баги',
-                colorId: 1
-            },
-            3: {
-                id: 3,
-                projectId: 2,
-                name: 'Общее',
-                colorId: 2
-            },
-            4: {
-                id: 4,
-                projectId: 3,
-                name: 'Баги',
-                colorId: 1
-            }
-        }
+        tasks: {},
+        projects: {},
+        groups: {}
     }
+    <?php for ($i = 0, $c = count($tasks); $i < $c; ++$i) : $task = $tasks[$i] ?>
+        // Рендерим задачу
+        _dashboardAppData.tasks[<?= $task['id'] ?>] = {
+            id: <?= $task['id'] ?>,
+            projectId: <?= $task['project_id'] ?>,
+            number: <?= $task['number'] ?>,
+            groupId: <?= $task['group_id'] ?>,
+            title: '<?= $task['title'] ?>'
+        }
+
+        // Рендерим группу, если еще не грузили ее
+        <?php if (!isset($groups[$task['group_id']])) : ?>
+            <?php
+            $groups[$task['group_id']] = true;
+            $group = Yii::$app->db->createCommand(
+                'SELECT name, color_id FROM issue_groups
+                WHERE id = ' . $task['group_id']
+            )->queryOne();
+            ?>
+            _dashboardAppData.groups[<?= $task['group_id'] ?>] = {
+                id: <?= $task['group_id'] ?>,
+                name: '<?= $group['name'] ?>',
+                colorId: <?= $group['color_id'] ?>
+            }
+        <?php endif ?>
+
+        // Рендерим проект, если еще не грузили его
+        <?php if (!isset($projects[$task['project_id']])) : ?>
+            <?php
+            $projects[$task['project_id']] = true;
+            $project = Yii::$app->db->createCommand(
+                'SELECT projects.name AS name,
+                        LEFT(project_descriptions.description, 50) AS description
+                    FROM projects INNER JOIN project_descriptions 
+                        ON projects.id = project_descriptions.project_id
+                    WHERE projects.id = ' . $task['project_id']
+            )->queryOne();
+            ?>
+            _dashboardAppData.projects[<?= $task['project_id'] ?>] = {
+                id: <?= $task['project_id'] ?>,
+                name: '<?= $project['name'] ?>',
+                description: '<?= str_replace(["\n", "\r"], ['\n', '\r'], $project['description']) ?>'
+            }
+        <?php endif ?>
+    <?php endfor ?>
 </script>
 
 <div id="dashboard-app"></div>

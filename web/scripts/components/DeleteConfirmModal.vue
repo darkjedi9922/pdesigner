95d5da46176e0f6929942dd8a2b153866df5a345
Jed Dark <darkjedi9922@gmail.com>
2020-06-21 17:15:25 +0300

    Fix DeleteConfirmModal icon align.

--- web/scripts/components/DeleteConfirmModal.vue
+++ web/scripts/components/DeleteConfirmModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="ui basic mini modal" ref="projectDeleteConfirmationModal">
-        <h2 class="ui icon header">
+        <h2 class="ui icon header center aligned">
             <i class="trash icon"></i>
             <div class="content">{{ title }}</div>
         </h2>
